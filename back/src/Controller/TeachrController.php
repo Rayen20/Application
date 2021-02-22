33b444afd4cfc184af0ab38e8aa03535c7dd95ee
Rayene Limem <rayane.limem@gmail.com>
2021-02-22 23:41:03 +0100

    post

---
+++ back/src/Controller/TeachrController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Teachr;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\TeachrFormType;
+
+class TeachrController extends AbstractController
+{  
+    /**
+     * @Route("/teachr", name="teachr")
+     */
+    public function index(): Response
+    {
+        return $this->render('teachr/index.html.twig', [
+            'controller_name' => 'TeachrController',
+        ]);
+    }
+
+    /**
+ * @Route("/add", name="add")
+ */
+public function add(Request $request): Response
+{
+    $teachr = new Teachr();
+    $form = $this->createForm(TeachrFormType::class, $teachr);
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid())
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($teachr);
+        $entityManager->flush();
+    }
+
+    return $this->render("teachr/teachr-form.html.twig", [
+        "form_title" => "Ajouter un objet teachr",
+        "form_teachr" => $form->createView(),
+    ]);
+}
+}
---
+++ back/src/Form/TeachrFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Teachr;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+class TeachrFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('date')
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Teachr::class,
+        ]);
+    }
+}
